59f254d1f55433e0513c41804e168546d1a5a3db
Mikhail Znak <mikhailznak@gmail.com>
2018-07-16 19:29:33 +0300

    Test 4 edit

--- task4/task4.py
+++ task4/task4.py
@@ -120,7 +120,7 @@ class WriteToLog(object):
         if self.config["network"] == "1":
             for i in self.data.net_adapter:
                 p3 += "interface: {ad}, " \
-                      "ip: {ip}, mask: {mask}, "\
+                      "ip: {ip}, mask: {mask}, " \
                      .format(ad=i,
                              ip=self.data.net_adapter[i][0].address,
                              mask=self.data.net_adapter[i][0].netmask)
