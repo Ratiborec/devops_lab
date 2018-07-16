c07d6972ed7867a6086c3c6fc1681c09aa0df362
Mikhail Znak <mikhailznak@gmail.com>
2018-07-16 19:57:27 +0300

    Test 6

--- task6/task25.py
+++ task6/task25.py
@@ -1,45 +1,45 @@
 def count_char(_name):
-	_dict = {}
-	for char in _name:
-		if _dict.get(char) is None:
-			_dict[char] = 1
-		else:
-			_dict[char] += 1
-	return _dict
+    _dict = {}
+    for char in _name:
+        if _dict.get(char) is None:
+            _dict[char] = 1
+        else:
+            _dict[char] += 1
+    return _dict
 
 
 def sort_chars(_dict):
-	if not isinstance(_dict, dict):
-		return "Incorrect data"
-	else:
-		_sorted_values = sorted(_dict.values(), reverse=True)
-		_sorted_keys = sorted(_dict.keys())
-		_new_dict = {}
-		for key in _sorted_keys:
-			_new_dict[key] = _dict[key]
-		_dict.clear()
+    if not isinstance(_dict, dict):
+        return "Incorrect data"
+    else:
+        _sorted_values = sorted(_dict.values(), reverse=True)
+        _sorted_keys = sorted(_dict.keys())
+        _new_dict = {}
+        for key in _sorted_keys:
+            _new_dict[key] = _dict[key]
+        _dict.clear()
 
-		for val in _sorted_values:
-			for i in _new_dict:
-				if _new_dict[i] == val:
-					_dict[i] = val
-		return _dict
+        for val in _sorted_values:
+            for i in _new_dict:
+                if _new_dict[i] == val:
+                    _dict[i] = val
+        return _dict
 
 
 def output(_dict):
-	if not isinstance(_dict, dict):
-		return "Incorrect data"
-	else:
-		_out_keys = list(_dict.keys())
-		_sorted_values = sorted(_dict.values(), reverse=True)
-		if len(_dict) == 0:
-			print("Empty data")
-		else:
-			for i in range(len(_dict)):
-				print("{0}  {1}".format(_out_keys[i], _sorted_values[i]))
+    if not isinstance(_dict, dict):
+        return "Incorrect data"
+    else:
+        _out_keys = list(_dict.keys())
+        _sorted_values = sorted(_dict.values(), reverse=True)
+        if len(_dict) == 0:
+            print("Empty data")
+        else:
+            for i in range(len(_dict)):
+                print("{0}  {1}".format(_out_keys[i], _sorted_values[i]))
 
 
 if __name__ == "__main__":
-	name = input("Enter the company name: ")
-	_dict = sort_chars(count_char(name))
-	output(_dict)
+    name = input("Enter the company name: ")
+    _dict = sort_chars(count_char(name))
+    output(_dict)
--- task6/task6.py
+++ task6/task6.py
@@ -5,96 +5,96 @@ import yaml
 
 
 def get_site_packages():
-	for i in sys.path:
-		if "site-packages" in i:
-			return i
-			break
+    for i in sys.path:
+        if "site-packages" in i:
+            return i
+            break
 
 
 def get_virt_env():
-	return os.environ["VIRTUAL_ENV"]
+    return os.environ["VIRTUAL_ENV"]
 
 
 def get_pip_version():
-	return pip.__version__
+    return pip.__version__
 
 
 def get_env():
-	return sys.executable
+    return sys.executable
 
 
 def get_path():
-	return sys.path[0]
+    return sys.path[0]
 
 
 def get_version():
-	return "{0}.{1}".format(str(sys.version_info.major),
-	                        str(sys.version_info.minor))
+    return "{0}.{1}".format(str(sys.version_info.major),
+                            str(sys.version_info.minor))
 
 
 def get_alias():
-	out = []
-	alias = os.popen("pyenv versions --bare --skip-aliases "
-	                 "| grep -e '[0-9].[0-9].[0-9]$'")
-	for i in alias.read().split():
-		out.append(i)
-	alias.close()
-	return out
+    out = []
+    alias = os.popen("pyenv versions --bare --skip-aliases "
+                     "| grep -e '[0-9].[0-9].[0-9]$'")
+    for i in alias.read().split():
+        out.append(i)
+    alias.close()
+    return out
 
 
 def get_pip_path():
-	get_path = os.popen("which pip")
-	pip_path = get_path.read().split()[0]
-	get_path.close()
-	return pip_path
+    get_path = os.popen("which pip")
+    pip_path = get_path.read().split()[0]
+    get_path.close()
+    return pip_path
 
 
 def get_pip_modules():
-	module = os.popen("pip list")
-	_tmp = module.read().split()
-	out = dict(zip(_tmp[4::2], _tmp[5::2]))
-	module.close()
-	return out
+    module = os.popen("pip list")
+    _tmp = module.read().split()
+    out = dict(zip(_tmp[4::2], _tmp[5::2]))
+    module.close()
+    return out
 
 
 def create_data_dict():
-	out = {}
-	out["path"] = get_path()
-	out["version"] = get_version()
-	out["site_packages"] = get_site_packages()
-	out["virt"] = get_virt_env()
-	out["all_versions"] = other_vesions()
-	out["pip_version"] = get_pip_version()
-	out["env"] = get_env()
-	out["alias"] = get_alias()
-	out["pip_path"] = get_pip_path()
-	out["modules"] = get_pip_modules()
-	return out
+    out = {}
+    out["path"] = get_path()
+    out["version"] = get_version()
+    out["site_packages"] = get_site_packages()
+    out["virt"] = get_virt_env()
+    out["all_versions"] = other_vesions()
+    out["pip_version"] = get_pip_version()
+    out["env"] = get_env()
+    out["alias"] = get_alias()
+    out["pip_path"] = get_pip_path()
+    out["modules"] = get_pip_modules()
+    return out
 
 
 def write_json():
-	_to_json = open("json.json", "w")
-	_to_json.write("{0}".format(create_data_dict()))
-	_to_json.close()
+    _to_json = open("json.json", "w")
+    _to_json.write("{0}".format(create_data_dict()))
+    _to_json.close()
 
 
 def write_yaml():
-	_to_yaml = open("yaml.yaml", "w")
-	_to_yaml.write("{0}".format(yaml.dump(create_data_dict(),
-	                                      default_flow_style=False)))
-	_to_yaml.close()
+    _to_yaml = open("yaml.yaml", "w")
+    _to_yaml.write("{0}".format(yaml.dump(create_data_dict(),
+                                          default_flow_style=False)))
+    _to_yaml.close()
 
 
 def other_vesions():
-	version = [i[:3] for i in get_alias()]
-	_dict = {}
-	for i in version:
-		command = os.popen("pyenv which python{0}".
-		                   format(i)).read().rstrip("\n")
-		_dict[i] = command
-	return _dict
+    version = [i[:3] for i in get_alias()]
+    _dict = {}
+    for i in version:
+        command = os.popen("pyenv which python{0}".
+                           format(i)).read().rstrip("\n")
+        _dict[i] = command
+    return _dict
 
 
 if __name__ == "__main__":
-	write_yaml()
-	write_json()
+    write_yaml()
+    write_json()
